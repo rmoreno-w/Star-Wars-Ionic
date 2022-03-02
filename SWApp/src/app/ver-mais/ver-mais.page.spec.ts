d86739146a26a77df09fa8dbb322df2a405e4a31
rmoreno-w <rmoreno.w@gmail.com>
2022-03-02 14:24:28 -0300

    Update Angular @V.11

--- SWApp/src/app/ver-mais/ver-mais.page.spec.ts
+++ SWApp/src/app/ver-mais/ver-mais.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { VerMaisPage } from './ver-mais.page';
@@ -7,7 +7,7 @@ describe('VerMaisPage', () => {
   let component: VerMaisPage;
   let fixture: ComponentFixture<VerMaisPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VerMaisPage ],
       imports: [IonicModule.forRoot()]
